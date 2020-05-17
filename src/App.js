c13c06afd005826df3dbf4f5076dc3c3148bd303
zainbits <hello.zain11@gmail.com>
2020-05-17 21:17:43 +0530

    Part-2 | Nesting Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import PageTwo from './pagetwo';
+
 
 function App() {
   return (
     <div className="App">
       <h1>My First React app</h1>
       <p>Welcome :)</p>
+      <PageTwo />
     </div>
   );
 }
---
+++ src/pagetwo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PageTwo extends Component {
+    render(){
+        return(
+            <div className="page">
+                <div>Name: Zain</div>
+                <div>Age: 21</div>
+                <div>Course: CSE</div>
+            </div>
+        )
+    }
+}
+
+export default PageTwo
