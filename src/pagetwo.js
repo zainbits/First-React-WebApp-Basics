70a705c3c006ba38398eb75eb4016bed1a3f69d8
zainbits <hello.zain11@gmail.com>
2020-05-18 18:15:29 +0530

    Part-6 | Conditional Output - 2

--- src/pagetwo.js
+++ src/pagetwo.js
@@ -2,24 +2,37 @@ import React from 'react';
 
 const Users = (props) => {
     //console.log(this.props);
+    // const users = props.users;
+    // const userList = users.map(user => {
+    //     if(user.age > 21){
+    //         return (
+    //             <div className="page" key={user.id}>
+    //                 <div>ID: {user.id}</div>
+    //                 <div>Name: {user.name}</div>
+    //                 <div>Age:{user.age}</div>
+    //                 <div>Course: {user.course}</div>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null;
+    //         }
+    // })
+
     const users = props.users;
-    const userList = users.map(user => {
-        if(user.age > 21){
-            return (
-                <div className="page" key={user.id}>
-                    <div>ID: {user.id}</div>
-                    <div>Name: {user.name}</div>
-                    <div>Age:{user.age}</div>
-                    <div>Course: {user.course}</div>
-                </div>
-            )
-        } else {
-            return null;
-            }
-    })
+    
     return(
         <div className="user-list">
-            {userList}
+            {
+            users.map(user => {
+                return user.age > 21 ? (
+                    <div className="page" key={user.id}>
+                        <div>ID: {user.id}</div>
+                        <div>Name: {user.name}</div>
+                        <div>Age:{user.age}</div>
+                        <div>Course: {user.course}</div>
+                    </div>
+        ) : null;
+    })}
         </div>
     )
 }
