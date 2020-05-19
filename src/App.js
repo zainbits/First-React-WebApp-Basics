37c6b18a55e2e27111a1010cb3bd8d657048308b
zainbits <hello.zain11@gmail.com>
2020-05-19 16:46:26 +0530

    Part-8 | Functions as Props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Users from './pagetwo';
 import AddUser from './AddUser';
 
 
-function App() {
-    const users = [
+class App extends Component {
+  state = {
+    users : [
         {name: 'Name1', age: 21, course: 'cse', id: 1},
         {name: 'Name2', age: 89, course: 'mes', id: 2},
         {name: 'Name3', age: 25, course: 'ddr', id: 3}
-    ];
+    ]
+  }
+
+    addUser = (user) => {
+      user.id = Math.random();
+      let users = [...this.state.users, user];
+      this.setState({
+        users : users
+      });
+    }
+    render() {
   return (
     <div className="App">
       <h1>My First React app</h1>
       <p>Welcome :)</p>
-      <Users users={users} />
-      <AddUser />
+      <Users users={this.state.users} />
+      <AddUser addUser={this.addUser}/>
     </div>
   );
+}
 }
 
 export default App;
